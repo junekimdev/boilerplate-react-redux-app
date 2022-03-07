42a50c775705377e62864a1c175bf7e917c5252a
June Kim <godlyjune@gmail.com>
2022-03-07 15:30:25 +0900

    Refactor process.browser since it's deprecated

--- controllers/store.ts
+++ controllers/store.ts
@@ -3,12 +3,15 @@ import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 import rootReducer from './reducers';
 
+const isClientside = () => typeof window !== 'undefined';
+const isNonProduction = () => process.env.NODE_ENV !== 'production';
+
 export const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
   const enhancer =
     // only client-side on non-production stage should enable redux debugger
-    process.browser && process.env.NODE_ENV !== 'production'
+    isClientside() && isNonProduction()
       ? compose(
           applyMiddleware(...middlewares),
           (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
