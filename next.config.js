7900719cc71daba7334750807331eeb7a0d79b64
June Kim <godlyjune@gmail.com>
2023-10-19 16:00:45 +0900

    Use default GTM_ID

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   env: {
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     // NEXT_PUBLIC_CMS_URL: process.env.NEXT_PUBLIC_CMS_URL,
-    NEXT_PUBLIC_GTM_ID: process.env.NEXT_PUBLIC_GTM_ID,
+    // NEXT_PUBLIC_GTM_ID: process.env.NEXT_PUBLIC_GTM_ID,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   eslint: { ignoreDuringBuilds: true },
